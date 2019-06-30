d87595e16a437c155d9b27704e71efacdd9143b4
Yukai Yang <yukai.yang@gmail.com>
2019-07-01 01:02:01 +0200

    make utils.go

--- mults.go
+++ mults.go
@@ -7,9 +7,9 @@ import (
 // MulTS is a struct for the multivariate time series
 type MulTS struct {
 	data      [][]float64
+	iTT       int
 	freq      int
-	start     [2]int
-	end       [2]int
+	rows      [][]int
 	vnames    []string
 	laglength int
 	dep       []int
@@ -25,6 +25,7 @@ func (ts *MulTS) SetData(data []float64, nvar int, vnames []string) error {
 	if iTT*nvar != len(data) {
 		return errors.New("dimension does not fit")
 	}
+	ts.iTT = iTT
 	ts.data = make([][]float64, nvar)
 	for i := 0; i < nvar; i++ {
 		ts.data[i] = make([]float64, iTT)
@@ -40,10 +41,42 @@ func (ts *MulTS) SetData(data []float64, nvar int, vnames []string) error {
 // SetFreq sets the frequency of the time series
 // start and end are 2-slices of int representing the start and end time points
 // if start and/or end are nil, they will be inferred
-func (ts *MulTS) SetFreq(freq int, start, end [2]int) error {
+func (ts *MulTS) SetFreq(freq int, start, end []int) error {
+	if ts.data == nil {
+		return errors.New("no data")
+	}
 	if freq < 1 {
 		return errors.New("wrong frequency")
 	}
+
+	if start != nil {
+		if len(start) < 2 {
+			return errors.New("start has less than two values")
+		}
+		if start[1] < 0 || start[1] >= freq {
+			return errors.New("start's freq is invalid")
+		}
+
+		ts.rows = make([][]int, ts.iTT)
+		makerows(ts.rows, start, ts.iTT, freq)
+
+	} else {
+		if end != nil {
+			if len(end) < 2 {
+				return errors.New("end has less than two values")
+			}
+			if end[1] < 0 || end[1] >= freq {
+				return errors.New("end's freq is invalid")
+			}
+
+			ts.rows = make([][]int, ts.iTT)
+			makerowsrev(ts.rows, end, ts.iTT, freq)
+		} else {
+			ts.rows = make([][]int, ts.iTT)
+			makerows(ts.rows, []int{0, 0}, ts.iTT, freq)
+		}
+	}
+
 	ts.freq = freq
 
 	return nil
---
+++ utils.go
@@ -0,0 +1,30 @@
+package mults
+
+/* utility functions */
+
+func makerows(rows [][]int, start []int, iTT, freq int) {
+	rows[0] = []int{start[0], start[1]}
+	for i := 1; i < iTT; i++ {
+		rows[i] = make([]int, 2)
+		rows[i][0] = rows[i-1][0]
+		rows[i][1] = rows[i-1][1] + 1
+		if rows[i][1] == freq {
+			rows[i][1]--
+			rows[i][0]++
+		}
+	}
+}
+
+func makerowsrev(rows [][]int, end []int, iTT, freq int) {
+	rows[iTT-1] = []int{end[0], end[1]}
+	for i := iTT - 2; i >= 0; i-- {
+		rows[i] = make([]int, 2)
+		rows[i][0] = rows[i+1][0]
+		rows[i][1] = rows[i+1][1] - 1
+		if rows[i][1] < 0 {
+			rows[i][1]++
+			rows[i][0]--
+		}
+	}
+
+}
